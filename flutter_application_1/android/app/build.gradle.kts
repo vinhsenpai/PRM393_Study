87bd766f95cdd87a95e4691be945928853ab2cfb
vinhsenpai <70082618+vinhsenpai@users.noreply.github.com>
2026-07-18 22:23:50 +0700

    commit

--- flutter_application_1/android/app/build.gradle.kts
+++ flutter_application_1/android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -45,4 +46,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-analytics")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
 }
